678573af6598db16b9bcf2b3dbcedf033356f814
Nina <nina.micleusanu@saep-ict.it>
2021-11-29 16:50:01 +0100

    Reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'reactiveForm';
+export class AppComponent implements OnInit {
+  projectStatus = ['Stable', 'Critical', 'Finished'];
+
+  projectForm = new FormGroup({
+    projectName: new FormControl('RCB', Validators.required),
+    email: new FormControl('admin@gmail.it', [
+      Validators.required,
+      Validators.email,
+    ]),
+    projectStatus: new FormControl(this.projectStatus, Validators.required),
+  });
+
+  ngOnInit() {}
+
+  onSubmitForm() {
+    console.log(this.projectForm);
+  }
 }
